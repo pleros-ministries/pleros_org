78b49b80cd89ab51f5c34a6dd0733f6ebe8a7352
oolutekunbi <oolutekunbi@gmail.com>
2026-06-17 18:35:00 +0100

    chore: remove unused YouTube debug API route

--- app/api/debug-yt/route.ts
+++
@@ -1,6 +0,0 @@
-import { getLatestYoutubeVideos } from "../../../../lib/homepage-feed";
-
-export async function GET() {
-  const videos = await getLatestYoutubeVideos(10);
-  return Response.json({ count: videos.length, videos });
-}
